15eeff7708a299f1d11176577bae53e1e285d96a
huabin.hong <121826725@qq.com>
2020-11-27 22:10:28 +0800

    多线程 1.1

--- src/thread/ClassExercise.java
+++ src/thread/ClassExercise.java
@@ -3,15 +3,32 @@ package thread;
 /*
     课堂练习
         1. 创建线程输出1~100之间的偶数，要求使用两种方式创建线程
+        2. 两个线程，一个线程输出1-10，一个线程输出10-1，轮流输出
  */
 public class ClassExercise{
     public static void main(String[] args) {
+//        exerciseOne();
+
+        execriseTwo();
+    }
+
+    // 课堂练习一
+    private static void exerciseOne(){
         ClassExercise.PrintDouble01 printDouble01 = new ClassExercise().new PrintDouble01();
         printDouble01.start();
 
         ClassExercise.PrintDouble02 printDouble02 = new ClassExercise().new PrintDouble02();
         Thread thread = new Thread(printDouble02);
         thread.start();
+    }
+
+    // 课堂练习二
+    private static void execriseTwo(){
+        ClassExercise.OneToTen oneToTen = new ClassExercise().new OneToTen();
+        new Thread(oneToTen).start();
+
+        ClassExercise.TenToOne tenToOne = new ClassExercise().new TenToOne();
+        new Thread(tenToOne).start();
     }
 
     public void printDouble(){
@@ -37,6 +54,36 @@ public class ClassExercise{
         @Override
         public void run() {
             printDouble();
+        }
+    }
+
+    // 1 - 10
+    class OneToTen implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 1; i < 11; i++) {
+                System.out.println(Thread.currentThread().getName() + "=======" + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 10 - 1
+    class TenToOne implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 10; i > 0; i--) {
+                System.out.println(Thread.currentThread().getName() + "=======" + i);
+                try {
+                    Thread.sleep(1010);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
